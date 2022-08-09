36811a6257697cf2573a60a1fa40e44df8b0f35c
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-08-09 14:17:25 +0530

    Create LineChartAgency.vue

---
+++ LineChartAgency.vue
@@ -0,0 +1,55 @@
+<script>
+  import { Line } from 'vue-chartjs'
+  export default {
+    extends: Line,
+    props:['chart'],
+    watch: {
+        chart () {
+            console.log(this.chart);
+            this.$data._chart.update(); 
+        },
+    },  
+    data () {
+      return {
+        chartData:{
+          labels: this.chart.date,
+          datasets: [ 
+            {
+              label: 'Agency',
+              data: this.chart.agencies,
+              fill: false,
+              borderColor: '#2554FF',
+              backgroundColor: '#2554FF',
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: true
+              }
+            }],
+            xAxes: [ {
+              gridLines: {
+                display: false
+              }
+            }]
+          },
+          legend: {
+            display: true
+          },
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      }
+    },
+    mounted () {
+      this.renderChart(this.chartData, this.options)
+    }
+  }
+</script>
